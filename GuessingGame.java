5191f2fab6e158bb0312774c3ff80831a5d802b7
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-14 10:21:25 -0700

    Update GuessingGame.java
    I changed the while loop to a "Do-While" loop.

--- GuessingGame.java
+++ GuessingGame.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 
 import static java.lang.System.out;
 
-public class GuessingGame { // Adapted from Dr. Burd's 6-1 program in the book, GuessAgain
+public class GuessingGame {
 
     public static void main(String[] args) {
         var keyboard = new Scanner(System.in);
 
         int numGuesses = 0; // counter for our guesses
         int ceilingNumber = 100; // constant for our max random number
-        int randomNumber = new Random().nextInt(ceilingNumber) + 1; // generates a random number to guess
-        int newFloor = 1; // This will update in the loop so the player knows the new min
-        int newCeiling = ceilingNumber; // This will update in the loop so the player knows the new max
+        int randomNumber = new Random().nextInt(ceilingNumber) + 1;
+        int newFloor = 1; // This will update the loop so the player knows the new min.
+        int newCeiling = ceilingNumber; // This will update every loop so the player knows the new max.
 
         out.println(" ************ ");
         out.println("Welcome to the Guessing Game");
@@ -20,10 +20,10 @@ public class GuessingGame { // Adapted from Dr. Burd's 6-1 program in the book, 
         out.println();
 
         out.print("Enter an int from 1 to " + ceilingNumber + ": ");
-        int inputNumber = keyboard.nextInt(); // creates the guess variable from keyboard input
-        numGuesses++; // Intial guess is tallied
+        int inputNumber = keyboard.nextInt(); // created the guess variable from keyboard input
+        numGuesses++; // Intial guess tallied
 
-        while (inputNumber != randomNumber) { // while loop on the condition the guess is not the random number
+        do { // do loop 
             out.println();
             out.print("Try again...");
 
@@ -38,7 +38,7 @@ public class GuessingGame { // Adapted from Dr. Burd's 6-1 program in the book, 
             out.print("Enter an int from " + newFloor + " to " + newCeiling +": "); // gives player new range to guess
             inputNumber = keyboard.nextInt();
             numGuesses++;
-        }
+        } while (inputNumber != randomNumber); // condition for loop
         out.print("You win after "); // displays number of guesses it took to wing
         out.println(numGuesses + " guesses.");
 
