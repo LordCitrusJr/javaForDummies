7a21f0fac80dcc560da18a90a78081ba385d2793
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-18 09:57:03 -0700

    Create foodProduct.java

---
+++ foodProduct.java
@@ -0,0 +1,8 @@
+public class foodProduct { // A public class to create instances in useFoodProduct.java
+    String foodName;
+    String foodType;
+    Integer foodWeight;
+    Double foodPrice;
+    Integer foodServing;
+    Integer foodCalories;
+}
