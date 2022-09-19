73487de9e38cf679abd4431645d92d94a551f085
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-19 13:43:34 -0700

    Update foodProduct.java

--- foodProduct.java
+++ foodProduct.java
@@ -1,8 +1,19 @@
-public class foodProduct { // A public class to create instances in useFoodProduct.java
+import static java.lang.System.out;
+
+public class foodProduct {
     String foodName;
     String foodType;
-    Integer foodWeight;
     Double foodPrice;
     Integer foodServing;
     Integer foodCalories;
+
+    public void display() { // creating a void method doesn't return anything. It's just called and ran.
+        out.println("~Food Data~");
+        out.println("Food: " + foodName);
+        out.println("Type: " + foodType);
+        out.println("Price: $" + foodPrice);
+        out.println("Servings Per: " + foodServing);
+        out.println("Calories Per Serving: " + foodCalories / foodServing);
+        out.println();
+    }
 }
