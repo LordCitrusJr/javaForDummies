df65ff49461bb940eaa6974f19375850a21ba297
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-20 09:17:23 -0700

    Update useFoodProduct.java

--- useFoodProduct.java
+++ useFoodProduct.java
@@ -15,24 +15,35 @@ public class useFoodProduct {
         peach.foodPrice = 1.09;
         peach.foodCalories = 69;
         peach.foodServing = 1;
+        peach.foodWeight = 55;
 
         apple.foodName = "Apple";
         apple.foodType = "Fruit";
         apple.foodPrice = 0.69;
         apple.foodCalories = 25;
         apple.foodServing = 1;
+        apple.foodWeight = 45;
 
         pizza.foodName = "Pizza";
         pizza.foodType = "Pie";
         pizza.foodPrice = 5.99;
         pizza.foodCalories = 2400;
         pizza.foodServing = 8;
+        pizza.foodWeight = 300;
 
         peach.display(); // This calls the display method and makes our code nice and neat.
+        out.println("Price per Gram: " + peach.priceByWeight(peach.foodPrice, peach.foodWeight));
+        out.println("Calories per Serving: " + peach.caloriesPerServing(peach.foodCalories, peach.foodServing));
         out.println();
+
         apple.display();
+        out.println("Price per Gram: " + apple.priceByWeight(apple.foodPrice, apple.foodWeight));
+        out.println("Calories per Serving: " + apple.caloriesPerServing(apple.foodCalories, apple.foodServing));
         out.println();
+
         pizza.display();
+        out.println("Price per Gram: " + pizza.priceByWeight(pizza.foodPrice, pizza.foodWeight));
+        out.println("Calories per Serving: " + pizza.caloriesPerServing(pizza.foodCalories, pizza.foodServing));
         out.println();
 
     }
