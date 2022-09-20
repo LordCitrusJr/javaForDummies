77af62379dddb6b341c23363bf4e71d8deb9e863
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-20 09:16:43 -0700

    Update foodProduct.java

--- foodProduct.java
+++ foodProduct.java
@@ -6,14 +6,18 @@ public class foodProduct {
     Double foodPrice;
     Integer foodServing;
     Integer foodCalories;
+    Integer foodWeight;
 
     public void display() { // creating a void method doesn't return anything. It's just called and ran.
         out.println("~Food Data~");
         out.println("Food: " + foodName);
         out.println("Type: " + foodType);
         out.println("Price: $" + foodPrice);
-        out.println("Servings Per: " + foodServing);
-        out.println("Calories Per Serving: " + foodCalories / foodServing);
-        out.println();
+        out.println("Total Calories: " + foodCalories);
+        out.println("Number of Servings: " + foodServing);
+        out.println("Weight of food (grams): " + foodWeight);
     }
+
+    public double priceByWeight(double price, int weight) {return price / weight; } // method to pass along parameters
+    public int caloriesPerServing(int cals, int servs) {return cals / servs; } // method to pass along parameters
 }
