c7f620f8bbb23d42f997ff34b078ea9a416dc343
LordCitrusJr <104928882+LordCitrusJr@users.noreply.github.com>
2022-09-22 09:37:24 -0700

    Update foodProduct.java
    Updated the code with setters and getters!

--- foodProduct.java
+++ foodProduct.java
@@ -1,14 +1,14 @@
 import static java.lang.System.out;
 
-public class foodProduct {
-    String foodName;
-    String foodType;
-    Double foodPrice;
-    Integer foodServing;
-    Integer foodCalories;
-    Integer foodWeight;
+public class foodProduct { // Setting these to private hides the fields and makes the program more safe
+    private String foodName;
+    private String foodType;
+    private Double foodPrice;
+    private Integer foodServing;
+    private Integer foodCalories;
+    private Integer foodWeight;
 
-    public void display() { // creating a void method doesn't return anything. It's just called and ran.
+    public void display() { // Creating a void method doesn't return anything. It's just called and ran.
         out.println("~Food Data~");
         out.println("Food: " + foodName);
         out.println("Type: " + foodType);
@@ -18,6 +18,24 @@ public class foodProduct {
         out.println("Weight of food (grams): " + foodWeight);
     }
 
-    public double priceByWeight(double price, int weight) {return price / weight; } // method to pass along parameters
-    public int caloriesPerServing(int cals, int servs) {return cals / servs; } // method to pass along parameters
+    protected Double priceByWeight(double price, int weight) {return price / weight; } // method to pass along parameters
+    protected Integer caloriesPerServing(int cals, int servs) {return cals / servs; } // method to pass along parameters
+    
+    public void setFoodName(String n) { foodName = n; } // Setters and Getters start with a default value anmd will only return values to food product if set
+    public String getFoodName() { return foodName; } // Once Set, it creates a public value for the foodProduct class
+
+    public void setFoodType(String f) { foodType = f; }
+    public String getFoodType() { return foodType; }
+
+    public void setFoodPrice(double p) { foodPrice = p; }
+    public Double getFoodPrice() { return foodPrice; }
+
+    public void setFoodServing(Integer s) { foodServing = s; }
+    public Integer getFoodServing() { return foodServing; }
+
+    public void setFoodCalories(Integer c) { foodCalories = c; }
+    public Integer getFoodCalories() { return foodCalories;}
+
+    public void setFoodWeight(Integer w) { foodWeight = w;}
+    public Integer getFoodWeight() { return foodWeight; }
 }
